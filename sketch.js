a12d234e4be6dfbd9546ff71a04933b3a89db683
manalan-km <manalananand@gmail.com>
2023-12-21 13:24:18 +0530

    init

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  //dynamically creates canvas based on window width and
+  //height
+  createCanvas(windowWidth, windowHeight);
+  //creates a black backgound
+  background(0);
+}
+
+function draw() {
+}
